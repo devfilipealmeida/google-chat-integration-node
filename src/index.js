6e4bf94ae4fed26fdd315ce4f8de1169ec44e825
devfilipealmeida <filipesilva504@gmail.com>
2024-03-19 12:30:27 -0300

    iniciando serviçois do express

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000!');
+})
