3ad6d903a54f6ddc862f8864923ff02e6a091e3e
devfilipealmeida <filipesilva504@gmail.com>
2024-03-21 03:27:22 -0300

    buscar tickets em aberto pelo email

--- app/controllers/TicketController.js
+++ app/controllers/TicketController.js
@@ -3,13 +3,13 @@ const ticketService = require('../services/TicketService');
 class TicketController {
     async getTickets(req, res) {
         try {
-                const tickets = await ticketService.getAllTickets();
-                res.status(200).json({ "data": tickets });
-            } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Internal Server Error' });
-            }
+            const tickets = await ticketService.getAllTickets();
+            res.status(200).json({ "data": tickets });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
+    }
 
     async createTicket(req, res) {
         const data = req.body;
